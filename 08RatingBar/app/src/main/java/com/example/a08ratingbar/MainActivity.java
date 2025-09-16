dc82a78ad002841047e4ae445e33ba23c3e4e82d
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-09-16 23:33:01 +0530

    RatingBar Test

--- 08RatingBar/app/src/main/java/com/example/a08ratingbar/MainActivity.java
+++ 08RatingBar/app/src/main/java/com/example/a08ratingbar/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.a08ratingbar;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +14,26 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    RatingBar ratingBar;
+    Button button;
+    TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
 
+        ratingBar.findViewById(R.id.ratingBar);
+        textView.findViewById(R.id.textView);
+        button.findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LayerDrawable stars = (LayerDrawable) ratingBar.getProgressDrawable();
+                stars.getDrawable(2).setColorFilter(Color.GREEN, PorterDuff.Mode.SRC_ATOP);
+                textView.setText("You Rated : " + ratingBar.getRating());
+            }
+        });
     }
 }
