1274580f2b9cac787ccd34529957c258b6f16e41
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-09-16 21:08:37 +0530

    SeekBar Test

---
+++ 04SeekBar/app/src/main/java/com/example/a04seekbar/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.a04seekbar;
+
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    SeekBar seekBar;
+    TextView textView;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        seekBar.findViewById(R.id.seekBar);
+        textView.findViewById(R.id.textViwe);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textView.setText(String.valueOf(progress));
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+}
