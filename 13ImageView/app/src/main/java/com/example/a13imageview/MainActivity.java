6cb31b2f6ef0d8ef6eaf7f0bc5c5a6bf33f5c004
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-09-23 00:04:34 +0530

    ImageView Text Complet

--- 13ImageView/app/src/main/java/com/example/a13imageview/MainActivity.java
+++ 13ImageView/app/src/main/java/com/example/a13imageview/MainActivity.java
@@ -1,24 +1,34 @@
 package com.example.a13imageview;
-
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView imageView;
+    private Button button;
+    private int current_Image_Index;
+
+    int[] images = {R.drawable.ic_launcher_background, R.drawable.ic_launcher_foreground};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        buttonClick();
+    }
+    public void buttonClick() {
+        imageView = findViewById(R.id.imageView2);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                current_Image_Index++;
+                current_Image_Index = current_Image_Index % images.length;
+                imageView.setImageResource(images[current_Image_Index]);
+            }
         });
     }
 }
