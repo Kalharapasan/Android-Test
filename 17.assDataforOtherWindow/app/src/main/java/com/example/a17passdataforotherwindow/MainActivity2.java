80ade2b04bbaf273838e24e504efc463d45d3168
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-09-24 21:45:52 +0530

    Complete Pass Data for Other Window text

--- 17.assDataforOtherWindow/app/src/main/java/com/example/a17passdataforotherwindow/MainActivity2.java
+++ 17.assDataforOtherWindow/app/src/main/java/com/example/a17passdataforotherwindow/MainActivity2.java
@@ -1,6 +1,10 @@
 package com.example.a17passdataforotherwindow;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,31 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    TextView textView;
+    Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main2);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        textView = findViewById(R.id.textView3);
+        button = findViewById(R.id.button2);
+
+        Intent intent = getIntent();
+        String data = intent.getStringExtra("data");
+        textView.setText(data);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
         });
+
+
+
+
+
     }
 }
