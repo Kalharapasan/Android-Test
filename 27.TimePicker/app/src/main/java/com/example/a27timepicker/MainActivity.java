50e7a7e1cc09b2cd6896b6944bdd044ef8403d68
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-11-04 00:18:18 +0530

    TimePicker

--- 27.TimePicker/app/src/main/java/com/example/a27timepicker/MainActivity.java
+++ 27.TimePicker/app/src/main/java/com/example/a27timepicker/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.a27timepicker;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,22 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
+    TimePicker timePicker;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        button = findViewById(R.id.buttonDate);
+        timePicker =findViewById(R.id.textClock2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(),timePicker.getCurrentHour()+":"+timePicker.getCurrentMinute(), Toast.LENGTH_SHORT).show();
+            }
         });
+
     }
 }
